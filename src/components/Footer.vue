4f33a019efe7561d048ec5e28367b74215782049
Volodymyr Troian <volodymyr.troian@intenselab.com>
2024-06-16 22:30:32 +0300

    Fixed bugs

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,8 @@
 <template>
   <footer class="bg-zinc-800 h-10">
-    <h2 class="text-white">Footer :)</h2>
+    <h2 class="text-white text-center mt-2">Footer :)</h2>
   </footer>
 </template>
 
 <script setup lang="ts">
-
 </script>
